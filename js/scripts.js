e3944eabda3cb2999c814e395e19d76358192751
Andrew Mickel <andrew.m.mickel@gmail.com>
2020-04-22 09:57:31 -0700

    fixed a typo in form id

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $("form#insurance").submit(function(event) {
+  $("form#tracker").submit(function(event) {
     var side1 = parseInt($("input#side1").val());
     var side2 = parseInt($("input#side2").val());
     var side3 = parseInt($("input#side3").val());
